7f527eed22d4feef5ba1534a41254821c9f6a41d
Greg Leppert <greg@leppert.me>
2016-11-22 08:35:04 -0500

    Switch to Apache v2 license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject pal/pal-hashers "0.1.0-SNAPSHOT"
   :description "A ClojureScript port of buddy-hashers"
   :url "https://github.com/leppert/pal-hashers"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
                  [com.cemerick/piggieback "0.2.1"]
